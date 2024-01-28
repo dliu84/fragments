1bc7002996d4d617c23ec8b107931c77630bc37c
刘迪 <mayflower@s-MacBook-Pro.local>
2024-01-27 21:34:22 -0500

    rewrite HTTP responses to use the createSuccessResponse() and createErrorResponse() functions in src/response.js, tests pass

--- src/routes/api/get.js
+++ src/routes/api/get.js
@@ -3,11 +3,21 @@
 /**
  * Get a list of fragments for the current user
  */
+
+const { createSuccessResponse } = require('../../response');
 module.exports = (req, res) => {
   // TODO: this is just a placeholder. To get something working, return an empty array...
-  res.status(200).json({
-    status: 'ok',
-    // TODO: change me
-    fragments: [],
-  });
+  // res.status(200).json({
+  //   status: 'ok',
+  //   // TODO: change me
+  //   fragments: [],
+  // });
+
+  // step 42
+  res.status(200).json(
+    createSuccessResponse({
+      status: 'ok',
+      fragments: [],
+    })
+  );
 };
--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,9 @@ const express = require('express');
 // Our authentication middleware
 const { authenticate } = require('../auth');
 
+// step 42
+const { createSuccessResponse } = require('../response');
+
 // version and author from package.json
 const { version, author } = require('../../package.json');
 
@@ -26,13 +29,22 @@ router.get('/', (req, res) => {
   // Client's shouldn't cache this response (always request it fresh)
   res.setHeader('Cache-Control', 'no-cache');
   // Send a 200 'OK' response
-  res.status(200).json({
-    status: 'ok',
-    author,
-    // Use your own GitHub URL for this!
-    githubUrl: 'https://github.com/dliu84/fragments',
-    version,
-  });
+  // res.status(200).json({
+  //   status: 'ok',
+  //   author,
+  //   // Use your own GitHub URL for this!
+  //   githubUrl: 'https://github.com/dliu84/fragments',
+  //   version,
+  // });
+
+  // step 42
+  res.status(200).json(
+    createSuccessResponse({
+      author,
+      githubUrl: 'https://github.com/dliu84/fragments',
+      version,
+    })
+  );
 });
 
 module.exports = router;
