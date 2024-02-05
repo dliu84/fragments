ecba2f3468706efb3bdb77ccbd186c20a57fdf91
刘迪 <mayflower@s-MacBook-Pro.local>
2024-02-05 16:50:11 -0500

    added getById.test.js and updated get.test.js

---
+++ tests/unit/getById.test.js
@@ -0,0 +1,29 @@
+const request = require('supertest');
+
+const app = require('../../src/app');
+
+describe('GET /v1/fragments/:id', () => {
+  test('unauthenticated requests are denied', () =>
+    request(app).get('/v1/fragments/:id').expect(401));
+
+  test('incorrect credentials are denied', () =>
+    request(app)
+      .post('/v1/fragments/:id')
+      .auth('invalid@email.com', 'incorrect_password')
+      .expect(401));
+
+  test('authenticated users get fragment data with the given id', async () => {
+    const postRes = await request(app)
+      .post('/v1/fragments')
+      .auth('user1@email.com', 'password1')
+      .set('Content-Type', 'text/plain')
+      .send('This is fragment');
+    const { id } = JSON.parse(postRes.text).fragment;
+
+    const getRes = await request(app)
+      .get(`/v1/fragments/${id}`)
+      .auth('user1@email.com', 'password1');
+    expect(getRes.statusCode).toBe(200);
+    expect(getRes.body.fragmentData).toEqual('This is fragment');
+  });
+});
