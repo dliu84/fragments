8fbf5b545c3bffc4bbaa1990395015160cef877e
刘迪 <mayflower@s-MacBook-Pro.local>
2024-02-13 11:30:51 -0500

    updated in post.js, completed a1

--- src/routes/api/post.js
+++ src/routes/api/post.js
@@ -28,10 +28,12 @@ module.exports = async (req, res) => {
 
     logger.debug(`New Fragment is created: ${fragment}`);
 
+    // const apiURL = new URL(req.protocol + '://' + req.headers.host) || process.env.API_URL;
     const apiURL = process.env.API_URL || new URL(req.protocol + '://' + req.headers.host);
+
     // Set headers
     //res.setHeader('Content-type', fragment.type);
-    res.setHeader('Location', `${apiURL}/v1/fragments/${fragment.id}`);
+    res.setHeader('Location', `${apiURL}v1/fragments/${fragment.id}`);
 
     // Return successful response
     res.status(201).json(
@@ -48,43 +50,3 @@ module.exports = async (req, res) => {
       );
   }
 };
-
-// module.exports = async (req, res) => {
-//   const user = req.user;
-//   const data = req.body;
-//   const type = req.headers['content-type'];
-//   //const apiURL = process.env.API_URL || `http://${req.headers.host}`;
-//   const apiURL = process.env.API_URL || new URL(req.protocol + '://' + req.headers.host);
-
-//   logger.debug(`POST debugging: ${req.body}`);
-//   logger.info(`The apiURL is: http://${req.headers.host}`);
-
-//   if (Fragment.isSupportedType(type)) {
-//     try {
-//       // Generate a new Fragment metadata record based on Request
-//       const fragment = new Fragment({ ownerId: user, type });
-
-//       // Store metadata and data
-//       await fragment.setData(data);
-
-//       logger.debug(`New Fragment is created: ${fragment}`);
-
-//       // Set headers
-//       res.setHeader('Content-type', fragment.type);
-//       res.setHeader('Location', `${apiURL}/v1/fragments/${fragment.id}`);
-
-//       // Return successful response
-//       res.status(201).json(
-//         createSuccessResponse({
-//           fragment,
-//         })
-//       );
-//     } catch (err) {
-//       logger.error(err.message, 'Something went wrong, need to fix...');
-//       res.status(500).json(createErrorResponse(401, err.message));
-//     }
-//   } else {
-//     logger.error(`${type} is not supported Content Type`);
-//     res.status(415).json(createErrorResponse(415, `${type} is not supported Content Type`));
-//   }
-// };
