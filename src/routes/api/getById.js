223219898dd478cbb7a64a32e80369d191a84ed4
刘迪 <mayflower@s-MacBook-Pro.local>
2024-03-01 13:23:59 -0500

    updated getById.js route

--- src/routes/api/getById.js
+++ src/routes/api/getById.js
@@ -1,24 +1,49 @@
 // src/routes/api/getById.js
 //const express = require('express');
 //const { createSuccessResponse } = require('../../response');
-//const { createErrorResponse } = require('../../response');
+const { createErrorResponse } = require('../../response');
 const { Fragment } = require('../../model/fragment');
 const logger = require('../../logger');
+const path = require('path');
 
 /**
  * Gets an authenticated user's fragment data with the given ID
  */
 module.exports = async (req, res) => {
-  //try {
-  logger.debug(`User and id are: ${req.user}, ${req.params.id}`);
-  const fragment = await Fragment.byId(req.user, req.params.id);
-  const fragmentData = await fragment.getData();
-  logger.debug(`fragment Data is:${fragmentData}`);
-  res.setHeader('Content-type', fragment.type);
-  //res.send(fragmentData).toString('utf-8');
-  res.send(fragmentData);
-  //res.status(200).json(createSuccessResponse({ fragmentData: fragmentData }));
-  //} catch (err) {
-  //res.status(404).json(createErrorResponse(404, err.message));
-  //}
+  // a1:
+  // logger.debug(`User and id are: ${req.user}, ${req.params.id}`);
+  // const fragment = await Fragment.byId(req.user, req.params.id);
+  // const fragmentData = await fragment.getData();
+  // logger.debug(`fragment Data is:${fragmentData}`);
+  // res.setHeader('Content-type', fragment.type);
+
+  // res.send(fragmentData);
+
+  // a2:
+  //let url = req.originalUrl || req.url;
+  let url = req.originalUrl;
+
+  const name2 = path.basename(url);
+  const ext2 = path.extname(url);
+  const nameWithoutExt2 = path.basename(name2, ext2);
+  req.params.id = nameWithoutExt2;
+
+  let fragment;
+  let data;
+
+  try {
+    fragment = new Fragment(await Fragment.byId(req.user, req.params.id));
+    data = await fragment.getData();
+
+    data = fragment.convertData(data, ext2);
+  } catch (err) {
+    logger.debug({ err }, 'Error on requesting Fragment');
+    return res.status(404).json(createErrorResponse(404, ': Error requesting fragment: ' + err));
+  }
+  //a2:
+  res.setHeader('Content-Type', fragment.type);
+  res.setHeader('Content-Length', fragment.size);
+
+  // use .send to send the result instead of json, because the buffer will be automatically converted
+  return res.status(200).send(data);
 };
