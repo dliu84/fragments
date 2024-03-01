56f5eec2068ac55388ed778f474f3959cb604539
刘迪 <mayflower@s-MacBook-Pro.local>
2024-03-01 13:28:35 -0500

    updated getinfo.js file

--- src/routes/api/getinfo.js
+++ src/routes/api/getinfo.js
@@ -4,16 +4,17 @@ const { Fragment } = require('../../model/fragment');
  * Get a list of fragments for the current user
  */
 
-const { createSuccessResponse } = require('../../response');
+const { createSuccessResponse, createErrorResponse } = require('../../response');
+
 module.exports = async (req, res) => {
   let fragment;
 
   try {
     fragment = await Fragment.byId(req.user, req.params.id);
   } catch (err) {
-    res.status(400).json('Error requesting fragment');
+    return res.status(400).json(createErrorResponse('Error requesting fragment'));
   }
   res.set('Content-Type', fragment.type);
 
-  res.status(200).json(createSuccessResponse({ fragment }));
+  return res.status(200).json(createSuccessResponse({ fragment }));
 };
