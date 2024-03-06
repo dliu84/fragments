9f502f09cdd27eba06201c6ed46960f6c9c01060
刘迪 <mayflower@s-MacBook-Pro.local>
2024-03-06 10:08:16 -0500

    updated test in getById.test.js

--- tests/unit/getById.test.js
+++ tests/unit/getById.test.js
@@ -1,28 +1,91 @@
+// const request = require('supertest');
+
+// const app = require('../../src/app');
+
+// describe('GET /v1/fragments/:id', () => {
+//   test('unauthenticated requests are denied', () =>
+//     request(app).get('/v1/fragments/:id').expect(401));
+
+//   test('incorrect credentials are denied', () =>
+//     request(app)
+//       .post('/v1/fragments/:id')
+//       .auth('invalid@email.com', 'incorrect_password')
+//       .expect(401));
+
+//   test('authenticated users get fragment data with the given id', async () => {
+//     const postRes = await request(app)
+//       .post('/v1/fragments')
+//       .auth('user1@email.com', 'password1')
+//       .set('Content-Type', 'text/plain')
+//       .send('This is fragment');
+//     const { id } = JSON.parse(postRes.text).fragment;
+//     const getRes = await request(app)
+//       .get(`/v1/fragments/${id}`)
+//       .auth('user1@email.com', 'password1');
+//     expect(getRes.statusCode).toBe(200);
+//     expect(getRes.text).toEqual('This is fragment');
+//   });
+// });
+
 const request = require('supertest');
+var path = require('path');
 
 const app = require('../../src/app');
+const logger = require('../../src/logger');
+var filename;
+beforeAll(async () => {
+  const res = await request(app)
+    .post('/v1/fragments')
+    .auth('user1@email.com', 'password1')
+    .set({ 'Content-Type': 'text/plain' })
+    .send('{"This": "is a test"}');
 
-describe('GET /v1/fragments/:id', () => {
+  filename = path.basename(res.headers.location);
+
+  logger.debug({ filename }, ' Fragment Id');
+});
+
+describe('GET /v1/fragments/${filename}', () => {
+  // If the request is missing the Authorization header, it should be forbidden
   test('unauthenticated requests are denied', () =>
-    request(app).get('/v1/fragments/:id').expect(401));
+    request(app).get(`/v1/fragments/${filename}`).expect(401));
 
+  // If the wrong username/password pair are used (no such user), it should be forbidden
   test('incorrect credentials are denied', () =>
     request(app)
-      .post('/v1/fragments/:id')
+      .get(`/v1/fragments/${filename}`)
       .auth('invalid@email.com', 'incorrect_password')
       .expect(401));
 
-  test('authenticated users get fragment data with the given id', async () => {
-    const postRes = await request(app)
-      .post('/v1/fragments')
-      .auth('user1@email.com', 'password1')
-      .set('Content-Type', 'text/plain')
-      .send('This is fragment');
-    const { id } = JSON.parse(postRes.text).fragment;
-    const getRes = await request(app)
-      .get(`/v1/fragments/${id}`)
+  test('authenticated users get a Fragment', async () => {
+    const res = await request(app)
+      .get(`/v1/fragments/${filename}`)
       .auth('user1@email.com', 'password1');
-    expect(getRes.statusCode).toBe(200);
-    expect(getRes.text).toEqual('This is fragment');
+
+    expect(res.statusCode).toBe(200);
+  });
+
+  test('authenticated users get a Fragment converted into html', async () => {
+    const res = await request(app)
+      .get(`/v1/fragments/${filename}.html`)
+      .auth('user1@email.com', 'password1');
+
+    expect(res.statusCode).toBe(200);
+  });
+
+  // test('authenticated users gets 415 for converting text into image', async () => {
+  //   const res = await request(app)
+  //     .get(`/v1/fragments/${filename}.png`)
+  //     .auth('user1@email.com', 'password1');
+
+  //   expect(res.statusCode).toBe(415);
+  // });
+
+  test('authenticated users gets 200 for converting text into json', async () => {
+    const res = await request(app)
+      .get(`/v1/fragments/${filename}.json`)
+      .auth('user1@email.com', 'password1');
+
+    expect(res.statusCode).toBe(200);
   });
 });
