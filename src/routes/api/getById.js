4b77e2425d5f017ae1fae0663345c105090e8ec6
刘迪 <mayflower@s-macbook-pro.home>
2024-03-20 20:24:02 -0400

    updated getById.js file, now can convert md fragment to html fragment, completed a2

--- src/routes/api/getById.js
+++ src/routes/api/getById.js
@@ -1,6 +1,3 @@
-// src/routes/api/getById.js
-//const express = require('express');
-//const { createSuccessResponse } = require('../../response');
 const { createErrorResponse } = require('../../response');
 const { Fragment } = require('../../model/fragment');
 const logger = require('../../logger');
@@ -10,32 +7,41 @@ const path = require('path');
  * Gets an authenticated user's fragment data with the given ID
  */
 module.exports = async (req, res) => {
-  // a2:
   let url = req.originalUrl;
-
   const name2 = path.basename(url);
-  const ext2 = path.extname(url);
+  let ext2 = path.extname(url);
   const nameWithoutExt2 = path.basename(name2, ext2);
   req.params.id = nameWithoutExt2;
 
   let fragment;
   let data;
-
   try {
     fragment = new Fragment(await Fragment.byId(req.user, req.params.id));
     data = await fragment.getData();
-
-    data = await fragment.convertData(data, ext2, fragment);
-
-    console.log('the converted fragment is: ' + data);
+    ext2 ? (ext2 = ext2.substring(1)) : '';
   } catch (err) {
-    logger.debug({ err }, 'Error on requesting Fragment');
+    logger.debug({ err }, 'Error requesting fragment');
     return res.status(404).json(createErrorResponse(404, ': Error requesting fragment: ' + err));
   }
-  //a2:
-  res.setHeader('Content-Type', fragment.type);
+
+  if (ext2) {
+    try {
+      data = await fragment.convertData(data, ext2, fragment);
+    } catch (err) {
+      return res
+        .status(415)
+        .json(createErrorResponse(415, 'Unsupported media type for conversion: ' + err));
+    }
+
+    res.setHeader(
+      'Content-Type',
+      fragment.type.substring(0, fragment.type.indexOf('/')) + '/' + ext2
+    );
+  } else {
+    res.setHeader('Content-Type', fragment.type);
+  }
+
   res.setHeader('Content-Length', fragment.size);
 
-  // use .send to send the result instead of json, because the buffer will be automatically converted
   return res.status(200).send(data);
 };
