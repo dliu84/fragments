e5822b4ae4cb161b89b31b059716e8249d8d8bea
刘迪 <mayflower@s-macbook-pro.home>
2024-04-14 15:12:55 -0400

    updated getById.test.js to include the conversion tests

--- tests/unit/getById.test.js
+++ tests/unit/getById.test.js
@@ -1,32 +1,3 @@
-// const request = require('supertest');
-
-// const app = require('../../src/app');
-
-// describe('GET /v1/fragments/:id', () => {
-//   test('unauthenticated requests are denied', () =>
-//     request(app).get('/v1/fragments/:id').expect(401));
-
-//   test('incorrect credentials are denied', () =>
-//     request(app)
-//       .post('/v1/fragments/:id')
-//       .auth('invalid@email.com', 'incorrect_password')
-//       .expect(401));
-
-//   test('authenticated users get fragment data with the given id', async () => {
-//     const postRes = await request(app)
-//       .post('/v1/fragments')
-//       .auth('user1@email.com', 'password1')
-//       .set('Content-Type', 'text/plain')
-//       .send('This is fragment');
-//     const { id } = JSON.parse(postRes.text).fragment;
-//     const getRes = await request(app)
-//       .get(`/v1/fragments/${id}`)
-//       .auth('user1@email.com', 'password1');
-//     expect(getRes.statusCode).toBe(200);
-//     expect(getRes.text).toEqual('This is fragment');
-//   });
-// });
-
 const request = require('supertest');
 var path = require('path');
 
@@ -46,11 +17,9 @@ beforeAll(async () => {
 });
 
 describe('GET /v1/fragments/${filename}', () => {
-  // If the request is missing the Authorization header, it should be forbidden
   test('unauthenticated requests are denied', () =>
     request(app).get(`/v1/fragments/${filename}`).expect(401));
 
-  // If the wrong username/password pair are used (no such user), it should be forbidden
   test('incorrect credentials are denied', () =>
     request(app)
       .get(`/v1/fragments/${filename}`)
@@ -73,19 +42,19 @@ describe('GET /v1/fragments/${filename}', () => {
     expect(res.statusCode).toBe(200);
   });
 
-  // test('authenticated users gets 415 for converting text into image', async () => {
-  //   const res = await request(app)
-  //     .get(`/v1/fragments/${filename}.png`)
-  //     .auth('user1@email.com', 'password1');
-
-  //   expect(res.statusCode).toBe(415);
-  // });
-
   test('authenticated users gets 200 for converting text into json', async () => {
     const res = await request(app)
       .get(`/v1/fragments/${filename}.json`)
       .auth('user1@email.com', 'password1');
 
     expect(res.statusCode).toBe(200);
+  });
+
+  test('authenticated users gets 415 for converting text into image', async () => {
+    const res = await request(app)
+      .get(`/v1/fragments/${filename}.png`)
+      .auth('user1@email.com', 'password1');
+
+    expect(res.statusCode).toBe(415);
   });
 });
